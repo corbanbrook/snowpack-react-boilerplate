4e7c0d457547704331ce338e28af1fc297184007
Corban Riley <corbanbrook@gmail.com>
2020-12-06 11:42:09 -0500

    Bootstrapped with Create Snowpack App (CSA)

---
+++ snowpack.config.js
@@ -0,0 +1,31 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    open: 'none',
+    port: 9000
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
